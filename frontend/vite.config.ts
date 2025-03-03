1a9cd3e0ec24a619d28ed76a086ba107a862147e
Your Name <you@example.com>
2025-03-03 15:36:28 +0300

    css importing error fixed.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,16 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
